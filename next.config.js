84e6593be592d0f840fc80f2683c62650f062c73
hizkia904 <115910774+hizkia904@users.noreply.github.com>
2023-05-09 11:20:00 +0700

    Tambah file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
+
+export const dbConfig = {
+  MYSQL_HOST: "localhost",
+  MYSQL_PORT: "3306",
+  MYSQL_DATABASE: "si apotek adora",
+  MYSQL_USER: "root",
+  MYSQL_PASSWORD: "",
+};
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import Script from "next/script";
+import "../styles/a.scss";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const getLayout = Component.getLayout || ((page) => page);
+  return getLayout(
+    <>
+      <Script src="https://use.fontawesome.com/releases/v5.3.1/js/all.js" />
+      <Component {...pageProps} />
+    </>
+  );
 }
